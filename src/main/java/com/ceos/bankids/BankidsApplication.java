e399d4b9aa779625722b06b186a40a0414b53817
eojinsarang joo <wndjwlstkfkd@naver.com>
2022-06-13 04:22:31 +0900

    initial commit

---
+++ src/main/java/com/ceos/bankids/BankidsApplication.java
@@ -0,0 +1,13 @@
+package com.ceos.bankids;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankidsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankidsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ceos/bankids/BankidsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ceos.bankids;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankidsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
