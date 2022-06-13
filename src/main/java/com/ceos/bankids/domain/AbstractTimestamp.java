d0038b395815f227956d3f6c4e29100f58d2701a
eojinsarang joo <wndjwlstkfkd@naver.com>
2022-06-14 03:42:40 +0900

    feat: abstract timestamp

---
+++ src/main/java/com/ceos/bankids/domain/AbstractTimestamp.java
@@ -0,0 +1,24 @@
+package com.ceos.bankids.domain;
+
+import java.sql.Timestamp;
+import javax.persistence.Column;
+import javax.persistence.EntityListeners;
+import javax.persistence.MappedSuperclass;
+import lombok.Getter;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+@Getter
+@MappedSuperclass
+@EntityListeners(value = {AuditingEntityListener.class})
+public class AbstractTimestamp {
+
+    @Column(name = "created_at", nullable = false)
+    @CreationTimestamp
+    private Timestamp createdAt;
+
+    @Column(name = "updated_at", nullable = false)
+    @UpdateTimestamp
+    private Timestamp updatedAt;
+}
