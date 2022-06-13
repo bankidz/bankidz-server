b490034c5897501b8c1167cdf5c955e072fb65b3
eojinsarang joo <wndjwlstkfkd@naver.com>
2022-06-14 03:40:30 +0900

    feat: common response

--- src/main/java/com/ceos/bankids/config/CommonControllerAdvice.java
+++ src/main/java/com/ceos/bankids/config/CommonControllerAdvice.java
@@ -1,2 +1,24 @@
-package com.ceos.bankids.config;public class CommonControllerAdvice {
+package com.ceos.bankids.config;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import com.ceos.bankids.exception.BaseException;
+
+@ControllerAdvice
+public class CommonControllerAdvice {
+
+    @ExceptionHandler(value = BaseException.class)
+    public ResponseEntity onKnownException(BaseException baseException) {
+        return new ResponseEntity<>(CommonResponse.onFailure(baseException.getStatusCode(),
+            baseException.getResponseMessage()), null, baseException.getStatusCode());
+    }
+
+    @ExceptionHandler(value = Exception.class)
+    public ResponseEntity onException(Exception exception) {
+        exception.printStackTrace();
+        return new ResponseEntity<>(CommonResponse.onFailure(HttpStatus.INTERNAL_SERVER_ERROR,
+            "서버 에러가 발생했습니다."), null, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
 }
--- src/main/java/com/ceos/bankids/config/CommonResponse.java
+++ src/main/java/com/ceos/bankids/config/CommonResponse.java
@@ -1,2 +1,27 @@
-package com.ceos.bankids.config;public class CommonResponse {
+package com.ceos.bankids.config;
+
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@EqualsAndHashCode
+@NoArgsConstructor
+@AllArgsConstructor
+public class CommonResponse<T> {
+
+    HttpStatus statusCode;
+    String responseMessage;
+    T data;
+
+
+    public static <T> CommonResponse<T> onSuccess(T data) {
+        return new CommonResponse<>(HttpStatus.OK, null, data);
+    }
+
+    public static CommonResponse onFailure(HttpStatus statusCode, String responseMessage) {
+        return new CommonResponse<>(statusCode, responseMessage, null);
+    }
 }
