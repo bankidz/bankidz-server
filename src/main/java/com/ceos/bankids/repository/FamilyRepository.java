de38e0aa3159b883d35125875ef3bcc9c0a5b0a0
eojinsarang joo <wndjwlstkfkd@naver.com>
2022-07-08 10:06:11 +0900

    feat: 가족 생성 후 정보 반환 #24

---
+++ src/main/java/com/ceos/bankids/repository/FamilyRepository.java
@@ -0,0 +1,8 @@
+package com.ceos.bankids.repository;
+
+import com.ceos.bankids.domain.Family;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface FamilyRepository extends JpaRepository<Family, Long> {
+
+}
--- src/main/java/com/ceos/bankids/service/FamilyServiceImpl.java
+++ src/main/java/com/ceos/bankids/service/FamilyServiceImpl.java
@@ -1,12 +1,15 @@
 package com.ceos.bankids.service;
 
+import com.ceos.bankids.domain.Family;
 import com.ceos.bankids.domain.FamilyUser;
 import com.ceos.bankids.domain.User;
 import com.ceos.bankids.dto.FamilyDTO;
 import com.ceos.bankids.dto.FamilyUserDTO;
+import com.ceos.bankids.repository.FamilyRepository;
 import com.ceos.bankids.repository.FamilyUserRepository;
 import java.util.List;
 import java.util.Optional;
+import java.util.UUID;
 import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -16,6 +19,7 @@ import org.springframework.transaction.annotation.Transactional;
 @RequiredArgsConstructor
 public class FamilyServiceImpl implements FamilyService {
 
+    private final FamilyRepository fRepo;
     private final FamilyUserRepository fuRepo;
 
     @Override
@@ -31,10 +35,23 @@ public class FamilyServiceImpl implements FamilyService {
 
             return familyDTO;
         } else {
+            String newFamilyCode = UUID.randomUUID().toString();
+            Family newFamily = Family.builder()
+                .code(newFamilyCode)
+                .build();
+            fRepo.save(newFamily);
 
+            FamilyUser newFamilyUser = FamilyUser.builder()
+                .user(user)
+                .family(newFamily)
+                .build();
+            fuRepo.save(newFamilyUser);
+
+            List<FamilyUserDTO> familyUserDTOList = getFamilyUserList(newFamily.getId());
+            FamilyDTO familyDTO = new FamilyDTO(newFamilyCode, familyUserDTOList);
+
+            return familyDTO;
         }
-
-        return null;
     }
 
     @Override
