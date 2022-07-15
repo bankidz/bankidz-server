a56f19e10d3b99a3712f793bbed626b1003952ce
eojinsarang joo <wndjwlstkfkd@naver.com>
2022-07-15 15:27:03 +0900

    del: JsonManagedReference #31

--- src/main/java/com/ceos/bankids/domain/User.java
+++ src/main/java/com/ceos/bankids/domain/User.java
@@ -1,7 +1,6 @@
 package com.ceos.bankids.domain;
 
 import com.ceos.bankids.exception.BadRequestException;
-import com.fasterxml.jackson.annotation.JsonManagedReference;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
@@ -67,15 +66,12 @@ public class User extends AbstractTimestamp implements UserDetails {
     @OneToOne(mappedBy = "user", fetch = FetchType.LAZY)
     private Parent parent;
 
-    @JsonManagedReference
     @OneToMany(mappedBy = "user")
     private List<ChallengeUser> challengeUserList;
 
-    @JsonManagedReference
     @OneToMany(mappedBy = "user")
     private List<FamilyUser> familyUserList;
 
-    @JsonManagedReference
     @OneToMany(mappedBy = "user")
     private List<Comment> commentList;
 
