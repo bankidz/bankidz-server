0eb891a4678ac524b5398fdf1979f63a160ae347
eojinsarang joo <wndjwlstkfkd@naver.com>
2022-07-18 19:19:57 +0900

    feat: 부모 검증, 자녀 정보 리스트로 반환 #48

--- src/main/java/com/ceos/bankids/service/FamilyService.java
+++ src/main/java/com/ceos/bankids/service/FamilyService.java
@@ -4,15 +4,20 @@ import com.ceos.bankids.domain.Family;
 import com.ceos.bankids.domain.User;
 import com.ceos.bankids.dto.FamilyDTO;
 import com.ceos.bankids.dto.FamilyUserDTO;
+import com.ceos.bankids.dto.KidListDTO;
 import java.util.List;
 import org.springframework.stereotype.Service;
 
 @Service
 public interface FamilyService {
 
+
     public FamilyDTO postNewFamily(User user);
 
     public List<FamilyUserDTO> getFamilyUserList(Family family);
 
     public FamilyDTO getFamily(User user);
+
+    public List<KidListDTO> getKidListFromFamily(User user);
+
 }
