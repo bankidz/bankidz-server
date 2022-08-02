280f6ebb4ada3b8536d7a4859ec0f2df0576bcda
eojinsarang joo <wndjwlstkfkd@naver.com>
2022-08-02 16:40:49 +0900

    del: 부모 마이페이지 수락률 빼고 제거 #106

--- src/main/java/com/ceos/bankids/dto/ParentDTO.java
+++ src/main/java/com/ceos/bankids/dto/ParentDTO.java
@@ -11,19 +11,13 @@ import lombok.ToString;
 @EqualsAndHashCode
 public class ParentDTO {
 
-    @ApiModelProperty(example = "15")
-    Long totalChallenge;
     @ApiModelProperty(example = "8")
     Long acceptedRequest;
     @ApiModelProperty(example = "10")
     Long totalRequest;
-    @ApiModelProperty(example = "300000")
-    Long savings;
 
     public ParentDTO(Parent parent) {
-        this.totalChallenge = parent.getTotalChallenge();
         this.acceptedRequest = parent.getAcceptedRequest();
         this.totalRequest = parent.getTotalRequest();
-        this.savings = parent.getSavings();
     }
 }
