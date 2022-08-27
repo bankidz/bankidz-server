a1371974cca6ab8f97203ff79567ee46bce5d5d0
김민준 <sanbonai06@naver.com>
2022-08-28 00:44:52 +0900

    refactor: expo-server-sdk 의존성 주입 및 로직 작성 #150

--- src/main/java/com/ceos/bankids/controller/NotificationController.java
+++ src/main/java/com/ceos/bankids/controller/NotificationController.java
@@ -1,9 +1,12 @@
 package com.ceos.bankids.controller;
 
+import com.ceos.bankids.constant.ChallengeStatus;
 import com.ceos.bankids.domain.Challenge;
 import com.ceos.bankids.domain.User;
+import com.ceos.bankids.service.ExpoNotificationServiceImpl;
 import com.ceos.bankids.service.NotificationServiceImpl;
 import io.swagger.annotations.ApiOperation;
+import java.util.HashMap;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -17,11 +20,21 @@ import org.springframework.web.bind.annotation.RestController;
 public class NotificationController {
 
     private final NotificationServiceImpl notificationService;
+    private final ExpoNotificationServiceImpl expoNotificationService;
 
     @ApiOperation(value = "돈길 상태 변경 알림")
     @GetMapping(produces = "application/json; charset=utf-8")
     public String notification(Challenge challenge, User authUser) {
-        notificationService.makeChallengeStatusMessage(challenge, authUser);
+//        notificationService.makeChallengeStatusMessage(challenge, authUser);
+        String title = "돈길 상태가 변경되었어요!";
+        String notificationBody =
+            challenge.getChallengeStatus() == ChallengeStatus.WALKING ? "제안된 돈길이 수락되었어요!"
+                : "제안된 돈길이 거절당했어요. 이유를 알아봐요.";
+        String token = "ExponentPushToken[EQBviQMfJm_1riRkM0KdjP]";
+        HashMap<String, Object> newMap = new HashMap<>();
+        newMap.put("challengeId", challenge.getId());
+        newMap.put("userId", authUser.getId());
+        expoNotificationService.sendMessage(token, title, notificationBody, newMap);
         return "NOTIFICATION SUCCESS";
     }
 }
