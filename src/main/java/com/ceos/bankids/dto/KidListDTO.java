119ae3017a7b0a7b234edcddac56312fba44efa2
김민준 <sanbonai06@naver.com>
2022-09-02 14:42:34 +0900

    chore: DTO 잘못 바꿔서 수정 #172

--- src/main/java/com/ceos/bankids/dto/KidListDTO.java
+++ src/main/java/com/ceos/bankids/dto/KidListDTO.java
@@ -13,6 +13,8 @@ public class KidListDTO {
 
     @ApiModelProperty(example = "1")
     Long kidId;
+    @ApiModelProperty(example = "주어랑")
+    String username;
     @ApiModelProperty(example = "true")
     Boolean isFemale;
     @ApiModelProperty(example = "1")
@@ -26,6 +28,7 @@ public class KidListDTO {
 
     public KidListDTO(User user) {
         this.kidId = user.getKid().getId();
+        this.username = user.getUsername();
         this.isFemale = user.getIsFemale();
         this.level = user.getKid().getLevel();
         this.savings = user.getKid().getSavings();
