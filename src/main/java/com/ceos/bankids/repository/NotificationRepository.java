d806437105dd0c720abaa6f880e9e3f04bc11d29
김민준 <sanbonai06@naver.com>
2022-09-06 19:23:54 +0900

    feat: 알림 관련 API 2개 추가 #189

--- src/main/java/com/ceos/bankids/repository/NotificationRepository.java
+++ src/main/java/com/ceos/bankids/repository/NotificationRepository.java
@@ -1,10 +1,12 @@
 package com.ceos.bankids.repository;
 
 import com.ceos.bankids.domain.Notification;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 
 public interface NotificationRepository extends
     JpaRepository<Notification, Long> {
 
+    public List<Notification> findAllByUserId(Long userId);
 }
