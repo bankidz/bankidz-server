db96b3f38cdca4c5588d3c080c65e7fa99d4e64c
김민준 <sanbonai06@naver.com>
2022-10-12 15:32:03 +0900

    chore: s3 관련 warn 로거 지우기

--- src/main/java/com/ceos/bankids/BankidsApplication.java
+++ src/main/java/com/ceos/bankids/BankidsApplication.java
@@ -7,6 +7,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class BankidsApplication {
 
+    static {
+        System.setProperty("com.amazonaws.sdk.disableEc2Metadata", "true");
+    }
+
     public static void main(String[] args) {
         SpringApplication.run(BankidsApplication.class, args);
         System.out.println("LocalDateTime.now() = " + LocalDateTime.now());
